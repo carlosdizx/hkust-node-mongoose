f5c530947e6d6fc97bae191106b0db0b45009cd8
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-24 20:22:22 -0500

    :sparkles: play with mongoose and model dish

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("Starting app with typescript");
+import connectMongodb from "./utils/mongodb.util";
+import DishModel from "./models/dish.model";
+const playWithMongodb = async () => {
+  const newDish = new DishModel({
+    name: "Pizza Hawaiian",
+    description: "Cheese, pineapple-topped and delicious sourdough mother",
+  });
+  console.log("Trying play with Mongodb");
+  const dishSaved = await newDish.save();
+  console.log(dishSaved);
+  const dishes = await DishModel.find({}).exec();
+  console.log(dishes);
+  await newDish.deleteOne({});
+};
+
+(async () => {
+  await connectMongodb();
+  await playWithMongodb();
+})();
