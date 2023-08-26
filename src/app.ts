2f20abe5029ac6c31cf253af7836dfd499f679e7
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-25 19:19:31 -0500

    :sparkles: change logic for make operations

--- src/app.ts
+++ src/app.ts
@@ -3,26 +3,49 @@ import DishModel from "./models/dish.model";
 const playWithMongodb = async () => {
   console.log("Trying play with Mongodb");
 
-  await DishModel.create({
+  DishModel.create({
     name: "Pizza Hawaiian",
     description: "Cheese, pineapple-topped and delicious sourdough mother",
-  });
-  console.log("created", await DishModel.findOne({}));
+  })
 
-  await DishModel.updateOne(
-    {},
-    {
-      $set: {
-        name: "Italian",
-        description:
-          "Cheese, peperoni, tomate paste and delicious sourdough mother",
-      },
-    }
-  ).exec();
+    .then((dish) => {
+      console.log("Created: ", dish);
+      console.log();
+      return DishModel.findOne({}).exec();
+    })
 
-  console.log("updated", await DishModel.findOne({}));
+    .then((dish) => {
+      if (dish) {
+        console.log("Found: ", dish);
+        return DishModel.updateOne(
+          { _id: dish._id },
+          {
+            $set: {
+              name: "Italian",
+              description:
+                "Cheese, peperoni, tomate paste and delicious sourdough mother",
+            },
+          }
+        ).exec();
+      }
+    })
 
-  await DishModel.deleteOne({}).exec();
+    .then((result) => {
+      if (result) return DishModel.findOne({}).exec();
+    })
+    .then((dish) => {
+      if (dish) {
+        dish.comments.push({
+          rating: 5,
+          comment: "I'm getting a sinking feeling!",
+          author: "Leonardo Di Caprio",
+        });
+        console.log("updated", dish);
+      }
+      return DishModel.deleteOne({});
+    })
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
 };
 
 (async () => {
